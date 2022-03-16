a6ca680d0f735da6df4747e3c93e7f241e1c63e8
Kamal <kamal.mohammed@outlook.com>
2022-03-15 18:01:33 -0600

    Updated auth

--- api/src/main/java/ca/bc/gov/educ/api/gradbusiness/EducGradBusinessApiApplication.java
+++ api/src/main/java/ca/bc/gov/educ/api/gradbusiness/EducGradBusinessApiApplication.java
@@ -66,7 +66,8 @@ public class EducGradBusinessApiApplication {
 		public void configure(WebSecurity web) {
 			web.ignoring().antMatchers("/api/v1/api-docs-ui.html",
 					"/api/v1/swagger-ui/**", "/api/v1/api-docs/**",
-					"/actuator/health", "/actuator/prometheus", "/health");
+					"/actuator/health", "/actuator/prometheus", "/health",
+					"/api/v1/document/**");
 		}
 	}
 }
