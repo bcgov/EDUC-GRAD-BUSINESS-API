cb89db20f77193d4ca0af6db557094c852664189
alexryba <alex.rybakov@gov.bc.ca>
2022-04-08 10:46:44 -0600

    STS integration of reporting data unit tests

--- api/src/test/java/ca/bc/gov/educ/api/gradbusiness/EducGradBusinessApiApplicationTests.java
+++ api/src/test/java/ca/bc/gov/educ/api/gradbusiness/EducGradBusinessApiApplicationTests.java
@@ -1,13 +1,136 @@
 package ca.bc.gov.educ.api.gradbusiness;
 
-import org.junit.jupiter.api.Test;
+import ca.bc.gov.educ.api.gradbusiness.service.GradBusinessService;
+import ca.bc.gov.educ.api.gradbusiness.util.EducGradStudentApiConstants;
+import ca.bc.gov.educ.api.gradbusiness.util.EducGraduationApiConstants;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.reactive.function.BodyInserter;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.function.Consumer;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.openMocks;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 class EducGradBusinessApiApplicationTests {
 
-	//@Test
-	void contextLoads() {
+	@MockBean
+	WebClient webClient;
+
+	@Mock
+	private WebClient.RequestHeadersSpec requestHeadersMock;
+
+	@Mock
+	private WebClient.RequestHeadersUriSpec requestHeadersUriMock;
+	@Mock
+	private WebClient.RequestBodySpec requestBodyMock;
+	@Mock
+	private WebClient.RequestBodyUriSpec requestBodyUriMock;
+	@Mock
+	private WebClient.ResponseSpec responseMock;
+
+	@Autowired
+	private EducGradStudentApiConstants educGradStudentApiConstants;
+
+	@Autowired
+	private EducGraduationApiConstants educGraduationApiConstants;
+
+	@Autowired
+	private GradBusinessService gradBusinessService;
+
+	@Before
+	public void setUp() {
+		openMocks(this);
 	}
 
+	@After
+	public void tearDown() {
+
+	}
+
+	@org.junit.jupiter.api.Test
+	public void testReportDataByPen() throws Exception {
+
+		String studentGradData = readFile("json/gradstatus.json");
+		assertNotNull(studentGradData);
+
+		String pen = "129382610";
+
+		String reportData = readFile("json/studentTranscriptReportData.json");
+		assertNotNull(reportData);
+
+		when(this.webClient.get()).thenReturn(this.requestHeadersUriMock);
+		when(this.requestHeadersUriMock.uri(String.format(educGraduationApiConstants.getGraduateReportDataByPenUrl(),"129382610"))).thenReturn(this.requestHeadersMock);
+		when(this.requestHeadersMock.headers(any(Consumer.class))).thenReturn(this.requestHeadersMock);
+		when(this.requestHeadersMock.retrieve()).thenReturn(this.responseMock);
+		when(this.responseMock.bodyToMono(byte[].class)).thenReturn(Mono.just(reportData.getBytes()));
+
+		ResponseEntity<byte[]> byteData = gradBusinessService.prepareReportDataByPen(pen, "accessToken");
+		assertNotNull(byteData);
+		assertTrue(byteData.getBody().length > 0);
+		String json = new String(byteData.getBody());
+		assertTrue(json.equals(reportData));
+
+	}
+
+	@org.junit.jupiter.api.Test
+	public void testReportDataByGraduationData() throws Exception {
+
+		String studentGradData = readFile("json/gradstatus.json");
+		assertNotNull(studentGradData);
+
+		String reportData = readFile("json/studentTranscriptReportData.json");
+		assertNotNull(reportData);
+
+		when(this.webClient.post()).thenReturn(this.requestBodyUriMock);
+		when(this.requestBodyUriMock.uri(educGraduationApiConstants.getGaduateReportDataByGraduation())).thenReturn(this.requestBodyUriMock);
+		when(this.requestBodyUriMock.headers(any(Consumer.class))).thenReturn(this.requestBodyMock);
+		when(this.requestBodyMock.contentType(any())).thenReturn(this.requestBodyMock);
+		when(this.requestBodyMock.body(any(BodyInserter.class))).thenReturn(this.requestHeadersMock);
+		when(this.requestHeadersMock.retrieve()).thenReturn(this.responseMock);
+		when(this.responseMock.bodyToMono(byte[].class)).thenReturn(Mono.just(reportData.getBytes()));
+
+		ResponseEntity<byte[]> byteData = gradBusinessService.prepareReportDataByGraduation(studentGradData, "accessToken");
+		assertNotNull(byteData);
+		assertTrue(byteData.getBody().length > 0);
+		String json = new String(byteData.getBody());
+		assertTrue(json.equals(reportData));
+
+	}
+
+	private String readFile(String jsonPath) throws Exception {
+		ClassLoader classLoader = getClass().getClassLoader();
+		InputStream inputStream = classLoader.getResourceAsStream(jsonPath);
+		StringBuffer sb = new StringBuffer();
+		InputStreamReader streamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+		BufferedReader reader = new BufferedReader(streamReader);
+		String line;
+		while ((line = reader.readLine()) != null) {
+			sb.append(line);
+		}
+		return sb.toString();
+	}
 }
