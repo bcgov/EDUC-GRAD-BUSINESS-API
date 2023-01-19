bee9f63188fab8e27003cb68d3ca40035f2284f4
chris.ditcher <Chris.Ditcher@QP.GOV.BC.CA>
2023-01-19 10:12:04 -0800

    Added Splunk support.

---
+++ api/src/main/java/ca/bc/gov/educ/api/gradbusiness/config/RequestResponseInterceptor.java
@@ -0,0 +1,54 @@
+package ca.bc.gov.educ.api.gradbusiness.config;
+
+import ca.bc.gov.educ.api.gradbusiness.util.EducGradBusinessApiConstants;
+import ca.bc.gov.educ.api.gradbusiness.util.LogHelper;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.AsyncHandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.time.Instant;
+
+@Component
+@Slf4j
+public class RequestResponseInterceptor implements AsyncHandlerInterceptor {
+
+    EducGradBusinessApiConstants constants;
+
+    @Autowired
+    public RequestResponseInterceptor(EducGradBusinessApiConstants constants) {
+        this.constants = constants;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // for async this is called twice so need a check to avoid setting twice.
+        if (request.getAttribute("startTime") == null) {
+            final long startTime = Instant.now().toEpochMilli();
+            request.setAttribute("startTime", startTime);
+        }
+        return true;
+    }
+
+    /**
+     * After completion.
+     *
+     * @param request  the request
+     * @param response the response
+     * @param handler  the handler
+     * @param ex       the ex
+     */
+    @Override
+    public void afterCompletion(@NonNull final HttpServletRequest request, final HttpServletResponse response, @NonNull final Object handler, final Exception ex) {
+        LogHelper.logServerHttpReqResponseDetails(request, response, constants.isSplunkLogHelperEnabled());
+        val correlationID = request.getHeader(constants.CORRELATION_ID);
+        if (correlationID != null) {
+            response.setHeader(constants.CORRELATION_ID, request.getHeader(constants.CORRELATION_ID));
+        }
+    }
+
+}
